17f74b8c3cc8fea54149cf4294fb9efe9e5ddbf4
sagarjain <sagarlinkites@gmail.com>
2019-05-16 10:22:23 +0530

    First commit

---
+++ app/src/main/java/com/sdktest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sdktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.relibit.pulsemetrics.PulseMetrics;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
